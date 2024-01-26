b793a647cd0b1a69f50d0deb1cdecb10b73899d8
Vincent <spotandroxy3@gmail.com>
2024-01-25 16:26:09 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-//import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
+//import Sidebar from './components/Sidebar'
+import Home from './pages/Home'
+import Quarter from './pages/Quarter'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Quarter />} />
+          <Route path='/home' exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
 
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+    return(
+        <Sidebar />
+    )
+}
+
