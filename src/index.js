55c0a19ed5059d9987047359fa743e33b7877b66
RedDitto <channathan345@gmail.com>
2024-02-01 19:12:03 -0800

    fixed App

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import MenuSimple from './components/MenuSimple';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MenuSimple/>
+    <App/>
   </React.StrictMode>
 );
 
