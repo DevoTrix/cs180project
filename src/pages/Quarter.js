a7203d93b073439f11722d8aae4931c1f7e65485
RedDitto <channathan345@gmail.com>
2024-02-10 04:16:57 -0800

    added background gif and ucr logo

--- src/pages/Quarter.js
+++ src/pages/Quarter.js
@@ -1,7 +1,14 @@
 import MenuSimple from '../components/MenuSimple'
+import UCRLogo from '../components/UCRLogo'
+import './Quarter.css'
+
 
 export default function Quarter() {
     return(
+        <body class="quarter">
         <MenuSimple />
+        <UCRLogo />
+        </body>
+        
     )
 }
