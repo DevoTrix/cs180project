4777ab2e1bda256bed04a508badfebcf406c1fe5
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2024-02-12 16:38:54 -0800

    did the calling to the database.

--- backend/helper/pullData.js
+++ backend/helper/pullData.js
@@ -1,15 +1,61 @@
+require('dotenv').config();
 const mysql = require("mysql2");
 
 
+// gets everything
 async function pullData(){
-    var query = "";
-    var values = [];
-    
+    var query = "SELECT * FROM Classes";
+    var conn = mysql.createConnection({
+        connectionLimit: 10,
+        host: process.env.host,
+        port: process.env.dbPort,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.dbName
+    })
+    var data;
+    await conn.promise().query(query)
+        .then(([rows, fields]) => {
+            data = JSON.stringify(rows);
+        })    
+        .catch((err) => {
+            console.error(err);
+         });
+
+    return data;
 }
 
+// searches
 async function pullSpecData(specification){
     var query = "";
-    var values = [];
+    const coursenumRegexPattern = "[a-zA-Z][a-zA-Z]([a-zA-Z]?)[0-9][0-9][0-9][a-cA-C]?";
+    // if match 
+    if(coursenumRegexPattern.test(specification)){
+        query = "SELECT * FROM Classes WHERE courseNum = ?"
+    }
+    else{
+        query = "SELECT * FROM Classes WHERE courseName LIKE '%' ||  ? || '%'"
+    }
+
+    var values = [specification];
+    var conn = mysql.createConnection({
+        connectionLimit: 10,
+        host: process.env.host,
+        port: process.env.dbPort,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.dbName
+    })
+    var data;
+    await conn.promise().query(query, values)
+        .then(([rows, fields]) => {
+            data = JSON.stringify(rows);
+        })    
+        .catch((err) => {
+            console.error(err);
+         });
+
+    return data;
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { ref } = require('yup');
+const {pullData, pullSpecData} = require('./helper/pullData');
 //end of imports
 //setting up apps
 const app = express();
@@ -14,7 +15,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mysql = require('mysql2');
 
+app.post('/api/search', async (req, res)=>{
+    const spec = req.body;
+    if(!spec){
+        try{
+            const data = await pullData();
+            res.status(200).json(data);
+        }
+        catch(error){
+            res.status(404).send({message:"Failed"});
+        }
+    }
+    else{
+        try{
+            const data = await pullSpecData(spec);
+            res.status(200).json(data);
+        }
+        catch(error){
+            res.status(404).send({message:"Failed"});
+        }
+    }
 
+})
 //final
 app.listen(5000, async ()=>{
     console.log("Server Running on http://localhost:5000")
