16ab55ed8628eff0016f61a6a002012b8f776c85
Joseph Chang <jchan443@ucr.edu>
2024-02-23 15:31:35 -0800

    organized some of the tests a little more and added some template tests to understand which tests needs to get done as front end finishes

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -1,16 +1,56 @@
-describe('functionality', () => {
-    it('renders the initial view of screen', () => {
+describe('drawer', () =>{
+  it('renders the initial view of screen', () => {
+    cy.visit('http://localhost:3000/home');
+    cy.get('.MuiToolbar-root > .MuiButtonBase-root').should("not.be.hidden");
+  })
+  it('opens drawer', () => {
+    cy.visit('http://localhost:3000/home');
+    cy.get(':nth-child(3) > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').should('be.hidden')
+    cy.get('.MuiToolbar-root > .MuiButtonBase-root').click();
+    cy.get(':nth-child(3) > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').should('not.be.hidden')
+  })
+  it('My Courses should open another bar containing classes over calendar when clicked', () => {
+    cy.visit('http://localhost:3000/home');
+    cy.log('still needs implementing')
+    // test if event is added
+    //test if event is repeating correctly
+  })
+  it('Add / delete should open another bar containing list view of added classesover calendar when clicked', () => {
+    cy.visit('http://localhost:3000/home');
+    cy.log('still needs implementing')
+    // test if event is added
+    //test if event is repeating correctly
+  })
+  it('logout should redirect back to quarter', () => {
+    cy.visit('http://localhost:3000/home');
+    cy.log('still needs implementing')
+    // test if event is added
+    //test if event is repeating correctly
+  })
+})
+describe('calendar', () => {
+    it('loads calendar', () => {
       cy.visit('http://localhost:3000/home');
-      cy.get('.MuiToolbar-root > .MuiButtonBase-root').should("not.be.hidden");
+      cy.get('.fc').should('exist');
     })
-    it('opens drawer', () => {
+    it('adds events to the calendar correctly', () => {
       cy.visit('http://localhost:3000/home');
-      cy.get(':nth-child(3) > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').should('be.hidden')
-      cy.get('.MuiToolbar-root > .MuiButtonBase-root').click();
-      cy.get(':nth-child(3) > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').should('not.be.hidden')
+      cy.log('still needs implementing')
+      // test if event is added
+      //test if event is repeating correctly
     })
-    
+    it('able to create a custom event', () => {
+      cy.visit('http://localhost:3000/home');
+      cy.log('still needs implementing')
+      // test if event is added
+      //test if event is repeating correctly
+    })
   describe('css', ()=>{
-
+    it('changes from light to dark correctly and vice versa', () => {
+      cy.visit('http://localhost:3000/home');
+      cy.log('still needs implementing')
+      // test if event is added
+      //test if event is repeating correctly
+    })
   })
 })
