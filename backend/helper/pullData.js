6775bc85cf97dd30a14359ca290ca745c49f8412
Joseph Chang <jchan443@ucr.edu>
2024-03-05 02:33:58 -0800

    fixed a minor bug

--- backend/helper/pullData.js
+++ backend/helper/pullData.js
@@ -25,18 +25,16 @@ async function pullSpecData(conn, specification, quarter) {
   const coursenumRegexPattern =/^([a-zA-Z]+)([a-zA-Z]?)([a-zA-Z]?)([0-9]{3})([a-cA-C]?)$/;
   let quart = '202420';
   const match = specification.match(coursenumRegexPattern);
-  console.log(match)
   let values;
   if(match){
     const num = specification.substring(match[1].length, specification.length);
-    console.log(num)
-    console.log(match[1])
     query ="SELECT * FROM Class WHERE subject = ? AND coursenum = ? AND term = ?";
     values = [match[1], num, quart]
   }else{
-    query ="SELECT * FROM Class WHERE courseName LIKE '%' ||  ? || '%' AND term = ?";
-    console.log("help")
-    values = [specification, quart]
+    var cntns = '%' + specification + '%';
+    query ="SELECT * FROM Class WHERE courseName LIKE ? AND term = ?";
+
+    values = [cntns, quart]
   }
 
   // conn.connect();
