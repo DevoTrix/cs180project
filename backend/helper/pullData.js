46be9fc572826668815374d57be1036f386f76ba
Joseph Chang <jchan443@ucr.edu>
2024-03-11 14:38:20 -0700

    testingSmth

--- backend/helper/pullData.js
+++ backend/helper/pullData.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 // gets everything
 async function pullData(conn, quarter) {
   var query = "SELECT * FROM Class WHERE term = ?";
-  var values = ['202420'];
+  var values = ["202420"];
   var data;
   await conn
     .promise()
@@ -22,26 +22,28 @@ async function pullData(conn, quarter) {
 // searches
 async function pullSpecData(conn, specification, quarter) {
   var query = "";
-  const coursenumRegexPattern =/^([a-zA-Z]+)([a-zA-Z]?)([a-zA-Z]?)([0-9]{3})([a-cA-C]?)$/;
-  let quart = '202420';
+  const coursenumRegexPattern =
+    /^([a-zA-Z]+)([a-zA-Z]?)([a-zA-Z]?)([0-9]{3})([a-cA-C]?)$/;
+  let quart = "202420";
   const match = specification.match(coursenumRegexPattern);
   let values;
-  if(match){
+  if (match) {
     const num = specification.substring(match[1].length, specification.length);
-    query ="SELECT * FROM Class WHERE subject = ? AND coursenum = ? AND term = ?";
-    values = [match[1], num, quart]
-  }else{
-    var cntns = '%' + specification + '%';
-    query ="SELECT * FROM Class WHERE courseName LIKE ? AND term = ?";
+    query =
+      "SELECT * FROM Class WHERE subject = ? AND coursenum = ? AND term = ?";
+    values = [match[1], num, quart];
+  } else {
+    var cntns = "%" + specification + "%";
+    query = "SELECT * FROM Class WHERE courseName LIKE ? AND term = ?";
 
-    values = [cntns, quart]
+    values = [cntns, quart];
   }
 
   // conn.connect();
   let data;
   await conn
     .promise()
-    .query(query,values)
+    .query(query, values)
     .then(([rows, fields]) => {
       data = JSON.stringify(rows);
     })
